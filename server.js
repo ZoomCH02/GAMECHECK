da231f65c489f12157563d64c87d563c00c81208
Ivan <70175295+ZoomCH02@users.noreply.github.com>
2024-04-20 14:09:35 +0300

    ver 0.9.1

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const multer = require('multer');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,6 +24,53 @@ app.use(session({
 }));
 
 app.use(bodyParser.json());
+
+
+const uploadPath = path.join(__dirname, 'public/uploads');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    // Генерация уникального имени файла
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+// Создание middleware для загрузки файла с помощью multer
+const upload = multer({ storage: storage });
+
+// Маршрут для изменения аватарки пользователя
+app.post('/uploadAvatar', upload.single('avatar'), (req, res) => {
+  // Проверка, вошел ли пользователь в систему
+  if (!req.session.user) {
+    return res.status(401).send('Пользователь не вошел в систему');
+  }
+
+  // Получение имени файла с его расширением из полного пути
+  const fileName = req.file ? path.basename(req.file.path) : null;
+
+  // Получение ID пользователя из сессии
+  const userId = req.session.user.id;
+
+  // Выполнение SQL-запроса для обновления записи пользователя с новым именем файла аватарки
+  const sql = `
+    UPDATE Users
+    SET avatar = ?
+    WHERE id = ?
+  `;
+  db.run(sql, [fileName, userId], (err) => {
+    if (err) {
+      console.error('Ошибка при обновлении аватарки пользователя:', err);
+      return res.status(500).send('Ошибка при обновлении аватарки пользователя');
+    }
+    // Возвращаем сообщение об успешном изменении аватарки
+    res.status(200).send('Аватарка пользователя успешно изменена');
+  });
+});
+
+
 
 
 app.post('/registerUser', (req, res) => {
@@ -62,32 +111,68 @@ app.post('/loginUser', (req, res) => {
   });
 });
 
+app.post('/addGame', (req, res) => {
+  // Проверка, вошел ли пользователь в систему
+  if (!req.session.user) {
+    return res.status(401).send('Пользователь не вошел в систему');
+  }
+
+  // Извлечение данных из тела запроса
+  const { game_id } = req.body;
+
+  // Получение ID пользователя из сессии
+  const user_id = req.session.user.id;
+
+  const currentDate = new Date();
+
+  // Получение дня, месяца и года
+  const day = currentDate.getDate().toString().padStart(2, '0');
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Месяц начинается с 0
+  const year = currentDate.getFullYear();
+
+  // Формирование строки в нужном формате
+  const formattedDate = `${day}.${month}.${year}`;
+
+  // Подготовка и выполнение SQL запроса для добавления игры в базу данных
+  const sql = `
+    INSERT INTO Playthroughs (user_id, game_id, rating, graphics_rating, plot_rating, atmosphere_rating, gameplay_rating, play_count, date_of_add)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+  db.run(sql, [user_id, game_id, 0, 0, 0, 0, 0, 1, formattedDate], (err) => {
+    if (err) {
+      console.error('Ошибка при добавлении игры в базу данных:', err);
+      return res.status(500).send('Ошибка при добавлении игры в базу данных');
+    }
+    res.status(200).send('Игра успешно добавлена в базу данных');
+  });
+});
+
+
 // Серверный маршрут для обновления рейтинга игры
 app.put('/game/:id/updateRating', (req, res) => {
   const gameId = req.params.id;
   const { plot_rating, gameplay_rating, atmosphere_rating, graphics_rating, rating } = req.body; // Предположим, что новые рейтинги передаются в теле запроса
 
   // Обновляем рейтинги игры в базе данных
-  db.run('UPDATE Playthroughs SET plot_rating = ?, gameplay_rating = ?, atmosphere_rating = ?, graphics_rating = ?, rating = ? WHERE game_id = ?', 
-         [plot_rating, gameplay_rating, atmosphere_rating, graphics_rating, rating, gameId], (err) => {
-    if (err) {
-      console.error('Ошибка при обновлении рейтингов игры:', err);
-      return res.status(500).send('Ошибка при обновлении рейтингов игры');
-    }
-    // После успешного обновления рейтингов, возвращаем обновленные данные игры
-    db.get('SELECT * FROM Playthroughs WHERE game_id = ?', [gameId], (err, gameData) => {
+  db.run('UPDATE Playthroughs SET plot_rating = ?, gameplay_rating = ?, atmosphere_rating = ?, graphics_rating = ?, rating = ? WHERE game_id = ?',
+    [plot_rating, gameplay_rating, atmosphere_rating, graphics_rating, rating, gameId], (err) => {
       if (err) {
-        console.error('Ошибка при выполнении запроса:', err);
-        return res.status(500).send('Ошибка при выполнении запроса');
+        console.error('Ошибка при обновлении рейтингов игры:', err);
+        return res.status(500).send('Ошибка при обновлении рейтингов игры');
       }
-      if (!gameData) {
-        return res.status(404).send('Игра не найдена');
-      }
-      res.json(gameData);
+      // После успешного обновления рейтингов, возвращаем обновленные данные игры
+      db.get('SELECT * FROM Playthroughs WHERE game_id = ?', [gameId], (err, gameData) => {
+        if (err) {
+          console.error('Ошибка при выполнении запроса:', err);
+          return res.status(500).send('Ошибка при выполнении запроса');
+        }
+        if (!gameData) {
+          return res.status(404).send('Игра не найдена');
+        }
+        res.json(gameData);
+      });
     });
-  });
 });
-
 
 
 app.get('/game/:id', (req, res) => {
@@ -108,10 +193,18 @@ app.get('/user', (req, res) => {
   // Проверяем, вошел ли пользователь
   if (req.session.user) {
     // Если пользователь вошел, отправляем его id и login
-    res.status(200).json({
-      id: req.session.user.id,
-      login: req.session.user.login
-    });
+    db.all('SELECT avatar FROM Users WHERE id=?', [req.session.user.id], (err, row) => {
+      if (err) {
+        console.log(err)
+      }
+      else {
+        res.status(200).json({
+          id: req.session.user.id,
+          login: req.session.user.login,
+          avatar: row[0].avatar
+        });
+      }
+    })
   } else {
     // Если пользователь не вошел, отправляем сообщение об ошибке
     res.status(401).send('Пользователь не вошел в аккаунт');
@@ -135,6 +228,29 @@ app.get('/user/games', (req, res) => {
       return res.status(500).send('Ошибка при выполнении запроса');
     }
     res.json(rows); // Отправляем данные игр пользователя в формате JSON
+  });
+});
+
+app.delete('/user/games/:gameId', (req, res) => {
+  // Проверка, вошел ли пользователь в систему
+  if (!req.session.user) {
+    return res.status(401).send('Пользователь не вошел в систему');
+  }
+
+  // Получение ID игры из параметров маршрута
+  const gameId = req.params.gameId;
+
+  // Получение ID пользователя из сессии
+  const userId = req.session.user.id;
+
+  // Подготовка и выполнение SQL запроса для удаления игры из базы данных
+  const sql = 'DELETE FROM Playthroughs WHERE game_id = ? AND user_id = ?';
+  db.run(sql, [gameId, userId], (err) => {
+    if (err) {
+      console.error('Ошибка при удалении игры из базы данных:', err);
+      return res.status(500).send('Ошибка при удалении игры из базы данных');
+    }
+    res.status(200).send('Игра успешно удалена из базы данных');
   });
 });
 
